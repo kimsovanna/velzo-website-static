cd4dc39827c09e8956a258885bf02e3c4b6ed221
sovanna <kimsovanna2@gmail.com>
2026-08-01 00:03:38 +0700

    refactor: mobile layout

---
+++ _nuxt/BCjh4BsK.js
@@ -0,0 +1,1 @@
+import{c as s,m as t}from"./Dvh6Asnt.js";const c=s({name:"QSpace",setup(){const e=t("div",{class:"q-space"});return()=>e}});export{c as _};
---
+++ _nuxt/hzUvpi6l.js
@@ -0,0 +1,1 @@
+import{ab as r}from"./Dvh6Asnt.js";const u=()=>{const{t:n,locale:s}=r();return{switchEnToKm:(t,e)=>s.value==="en"?t:e,translate:t=>n(t)}};export{u};
