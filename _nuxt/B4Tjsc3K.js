6892a66924d59ce3d3363d42d89d28180e24a354
sovanna <kimsovanna2@gmail.com>
2026-08-06 23:57:24 +0700

    refactor: mobile layout

---
+++ _nuxt/B4Tjsc3K.js
@@ -0,0 +1,1 @@
+import{u as a,a as i}from"./CU8h_zfO.js";import{d as n,z as s,E as r}from"./C4nXMQ2J.js";const u=n({name:"QResponsive",props:a,setup(e,{slots:o}){const t=i(e);return()=>s("div",{class:"q-responsive"},[s("div",{class:"q-responsive__filler overflow-hidden"},[s("div",{style:t.value})]),s("div",{class:"q-responsive__content absolute-full fit"},r(o.default))])}});export{u as _};
---
+++ _nuxt/CdtORI-3.js
@@ -0,0 +1,1 @@
+import{b7 as r}from"./C4nXMQ2J.js";const s=r("homeSearch",{state:()=>({search:""}),getters:{getSearch:e=>e.search},actions:{setSearch(e){this.search=e}}});export{s as u};
