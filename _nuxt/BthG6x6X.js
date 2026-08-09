2c2e82349d17120bb63b22352a105b01913a78f5
sovanna <kimsovanna2@gmail.com>
2026-08-09 15:58:55 +0700

    rename: useAPI to useApi

---
+++ _nuxt/BthG6x6X.js
@@ -0,0 +1,1 @@
+import{b7 as r}from"./2Yff7Y98.js";const s=r("homeSearch",{state:()=>({search:""}),getters:{getSearch:e=>e.search},actions:{setSearch(e){this.search=e}}});export{s as u};
---
+++ _nuxt/DstfcMmH.js
@@ -0,0 +1,1 @@
+import{d as s,z as t}from"./2Yff7Y98.js";const n=s({name:"QSpace",setup(){const e=t("div",{class:"q-space"});return()=>e}});export{n as _};
